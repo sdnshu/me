bbc279104a1f53957ba9c7af34cfe521220f3634
sdnshu <sudhanshuneravati055@gmail.com>
2025-10-17 22:45:54 +0530

    style: add poppins font

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ const RootLayout = ({
 }>) => {
     return (
         <html lang="en" suppressHydrationWarning>
-            <body>
+            <body className='poppins-regular'>
                 <ThemeProvider
                     attribute="class"
                     defaultTheme="dark"
